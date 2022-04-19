bc1c07223e73656cfcc3dd90bbea0db90e02994b
Sage Skaff <skaffsage@gmail.com>
2022-04-19 09:24:26 -0700

    Add add_item test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,3 +1,5 @@
+require "./lib/item"
+require "./lib/attendee"
 require "./lib/auction"
 require "rspec"
 
@@ -11,6 +13,15 @@ RSpec.describe Auction do
     it "has no items by default" do
       auction = Auction.new
       expect(auction.items).to eq []
+    end
+
+    it "can add items" do
+      auction = Auction.new
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      auction.add_item(item1)
+      auction.add_item(item2)
+      expect(auction.items).to eq [item1, item2]
     end
   end
 end
