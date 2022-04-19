805cdd29e502a6895aefbca9af0ed31eecfdb39d
Jim Riddle <jimriddle0@gmail.com>
2022-04-19 11:43:49 -0500

    wrote test for closing bidding on an item

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -125,4 +125,38 @@ RSpec.describe Auction do
       expect(auction.bidders).to eq(["Megan", "Bob", "Mike"])
     end
 
+    it 'can close bidding on an item' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+      auction = Auction.new
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee1, 22)
+      item1.add_bid(attendee2, 20)
+      item4.add_bid(attendee3, 50)
+
+      item3.add_bid(attendee2, 15)
+
+      expect(item1.bids.count).to eq(2)
+
+      item1.close_bidding
+
+      item1.add_bid(attendee3, 70)
+
+      expect(item1.bids.count).to eq(2)
+    end
+
 end
