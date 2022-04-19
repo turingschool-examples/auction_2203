7da7bb9d230a19b4063c4685b283e4c0389cd6aa
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 10:18:22 -0600

    Test: add items test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Auction do
     expect(auction).to be_an_instance_of(Auction)
     expect(auction.items).to eq([])
   end
+
+  it 'can add items' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    auction.add_item(item1)
+    auction.add_item(item2)
+    expect(auction.items).to eq([item1, item2])
+  end
+
 end
