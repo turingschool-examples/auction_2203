c14ff8608809bd19bb2956d6b4dc71224adf94dc
Sage Skaff <skaffsage@gmail.com>
2022-04-19 10:49:37 -0700

    Add bidder_info test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -103,5 +103,27 @@ RSpec.describe Auction do
       item3.add_bid(attendee2, 15)
       expect(auction.bidders).to eq(["Bob", "Megan", "Mike"])
     end
+
+    it "can list bidder info" do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      auction = Auction.new
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15)
+      expect(auction.bidder_info).to eq({})
+    end
   end
 end
