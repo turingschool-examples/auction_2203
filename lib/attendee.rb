5707671a99ff9d6d03e3c1e4c0b59bf15f9ad6e3
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-04-19 12:32:48 -0400

    feat: create auction class and tests

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,1 +1,12 @@
-class 
+class Attendee
+attr_reader :name, :budget
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @budget = attributes[:budget]
+  end
+
+  def budget
+    @budget.delete('$').to_i
+  end
+end
