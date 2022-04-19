4658b570731b9bfc692ac0f371e840d63c55d665
Lucas Colwell <colwellc37@gmail.com>
2022-04-19 10:47:44 -0600

    Add vars for it 2, write init test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -31,4 +31,24 @@ RSpec.describe Auction do
 
     expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
+
+  describe 'Iteration II' do
+    before :each do
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+    end
+
+    it 'starts with no bids' do
+      expect(@item1.bids).to eq({})
+    end
+  end
 end
