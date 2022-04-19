ad61284e272cd57570f954c01877890896b07ff3
Saba Bhamidipati <saba.bhamidipati@gmail.com>
2022-04-19 13:06:40 -0400

    feat: bc I'm shocked I got this far

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,21 @@
 class Item
-attr_reader :name
+attr_reader :name, :bids
 
   def initialize(name)
     @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, bid)
+    @bids[attendee] = bid
+  end
+
+  def current_high_bid
+    arr = []
+    @bids.map do |attendee, bid|
+      # require "pry"; binding.pry
+      arr << bid
+    end
+    arr.max
   end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -16,14 +16,42 @@ RSpec.describe do
 
   expect(auction.items).to eq([])
 
-  item1 = Item.new('Chalkware Piggy Bank')
-  item2 = Item.new('Bamboo Picture Frame')
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
 
-  auction.add_item(item1)
-  auction.add_item(item2)
+    auction.add_item(item1)
+    auction.add_item(item2)
 
   expect(auction.items).to eq([item1, item2])
   expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
 
+  it 'can take bids track items calculate revenue' do
+
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+  expect(item1.bids).to eq({})
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+  expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+  expect(item1.current_high_bid).to eq(22)
+  end
 end
