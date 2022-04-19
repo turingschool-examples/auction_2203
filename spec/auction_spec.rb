c8078ab02616192faea378d44ed0ec5b13176184
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-19 10:52:21 -0700

    Add test for add auction items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -3,26 +3,52 @@ require './lib/auction'
 require './lib/attendee'
 
 RSpec.describe Auction do
-  before :each do
-    @auction = Auction.new
-    @item1 = Item.new('Chalkware Piggy Bank')
-    @item2 = Item.new('Bamboo Picture Frame')
+  describe 'iteration I' do
+    before :each do
+      @auction = Auction.new
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    it 'exists and has attributes' do
+      expect(@auction). to be_a Auction
+      expect(@auction.items).to eq []
+    end
+
+    it 'can add auction items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.items).to eq [@item1, @item2]
+    end
+
+    it 'can name auction items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.item_names).to eq ["Chalkware Piggy Bank", "Bamboo Picture Frame"]
+    end
   end
 
-  it 'exists and has attributes' do
-    expect(@auction). to be_a Auction
-    expect(@auction.items).to eq []
-  end
+  describe 'iteration II' do
+    before :each do
+      @auction = Auction.new
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    end
 
-  it 'can add auction items' do
-    @auction.add_item(@item1)
-    @auction.add_item(@item2)
-    expect(@auction.items).to eq [@item1, @item2]
-  end
-
-  it 'can name auction items' do
-    @auction.add_item(@item1)
-    @auction.add_item(@item2)
-    expect(@auction.item_names).to eq ["Chalkware Piggy Bank", "Bamboo Picture Frame"]
+    it 'can add auction items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      
+      expect(@item1.bids).to eq {}
+    end
   end
 end
