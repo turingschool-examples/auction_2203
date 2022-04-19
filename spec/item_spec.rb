b1e903771a1e76392c4cc2838f57a44b867244d9
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-19 15:12:33 -0500

    Fix: Fixed close bids test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -17,6 +17,19 @@ RSpec.describe Item do
 
   it "can close bids" do
     item1 = Item.new('Chalkware Piggy Bank')
-    expect(item1).to be_an_instance_of(Item)
+
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+
+    item1.close_bidding
+
+    item1.add_bid(attendee3, 70)
+
+    expcected_hash = {attendee2 => 20, attendee1 => 22}
+    expect(item1.bids).to eq(expcected_hash)
   end
 end
