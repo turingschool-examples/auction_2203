ebd1034210289ab3f46213fae28536c68d7432f7
Sage Skaff <skaffsage@gmail.com>
2022-04-19 09:41:40 -0700

    Add current_high_bid test

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -29,5 +29,14 @@ RSpec.describe Item do
       item1.add_bid(attendee1, 22)
       expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
     end
+
+    it "can determine the current high bid" do
+      item1 = Item.new('Chalkware Piggy Bank')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      expect(item1.current_high_bid).to eq(22)
+    end
   end
 end
