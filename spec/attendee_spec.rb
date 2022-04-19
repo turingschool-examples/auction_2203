72bf30a07f8ea046d13cea9b17eb6b2957115f3f
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 10:11:37 -0600

    Test: attendee exists

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,12 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  it 'exists and has attributes' do
+    attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    expect(attendee).to be_an_instance_of(Attendee)
+    expect(attendee.name).to eq("Megan")
+    expect(attendee.budget).to eq(50)
+  end
+end
