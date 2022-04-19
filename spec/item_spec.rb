98d97796ee80f8ef37ea908708f97722fbecc57a
Deannah Burke <deannah@bierstadtlager.com>
2022-04-19 10:08:28 -0600

    Item spec and class initialize

---
+++ spec/item_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Item do
+  before :each do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  it "exists" do
+
+    expect(@item1).to be_a(Item)
+  end
+
+  it "has attributes" do
+
+    expect(@item1.name).to eq('Chalkware Piggy Bank')
+  end
+end
