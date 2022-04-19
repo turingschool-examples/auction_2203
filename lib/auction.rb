feb7cf87f7384473fb0124073c4caf1a05b014a9
Andrew King <dev.andrewking@gmail.com>
2022-04-19 12:16:15 -0500

    add bidders method

--- lib/auction.rb
+++ lib/auction.rb
@@ -28,5 +28,17 @@ class Auction
     sum
   end
 
+  # Was a little unsure if bidders was supposed to be an
+  # attribute for auction, would have made it an array
+  # that gets shoveled into each time a bid is placed
+  # and then use something like .uniq to remove duplicates
+  def bidders
+    @items.flat_map do |item|
+      item.bids.map do |bid|
+        bid[0].name
+      end
+    end.uniq
+  end
+
 
 end
