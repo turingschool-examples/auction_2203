1d49aae65a32a50897b0a6fca76c7c0d490154e1
Sage Skaff <skaffsage@gmail.com>
2022-04-19 09:38:12 -0700

    Add add_bid test

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,9 @@
 class Item
-  attr_reader :name
+  attr_reader :name,
+              :bids
 
   def initialize(name)
+    @bids = {}
     @name = name
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,4 +1,5 @@
 require "./lib/item"
+require "./lib/attendee"
 require "rspec"
 
 RSpec.describe Item do
@@ -17,7 +18,16 @@ RSpec.describe Item do
   describe "Iteration 2" do
     it "has no bids by default" do
       item1 = Item.new('Chalkware Piggy Bank')
-      expect(item1.bids).to eq {}
+      expect(item1.bids).to eq({})
+    end
+
+    it "can add bids" do
+      item1 = Item.new('Chalkware Piggy Bank')
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
     end
   end
 end
