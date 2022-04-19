0db2636447390fc0f237732f5a8b48a93cc20d6e
Bryce Wein <bryce.wein@gmail.com>
2022-04-19 12:03:39 -0400

    Test: Item initialize test

---
+++ spec/item_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './lib/item'
+
+desscribe Item do
+  it 'exists with attributes' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    expect(item1).to be_a(Item)
+    expect(item1.name).to eq('Chalkware Piggy Bank')
+  end
+end
