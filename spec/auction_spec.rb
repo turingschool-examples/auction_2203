7d75d04438e4933b68750911a891b3a5146111ec
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 10:31:59 -0600

    Test: add attendees

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -27,4 +27,45 @@ RSpec.describe Auction do
     expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
 
+  it 'can add attendees' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    expect(auction.attendees).to eq([])
+    auction.add_attendee(attendee1)
+    auction.add_attendee(attendee2)
+    auction.add_attendee(attendee3)
+    expect(auction.attendees).to eq([attendee1, attendee2, attendee3])
+  end
+
+
+  xit 'can add bids' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    expect(auction.bids).to eq({})
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    expect(auctiion.bids).to eq({attendee2 => 20, attendee1 => 22})
+  end
+
+
 end
