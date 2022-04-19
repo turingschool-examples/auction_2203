fcf002a8c5a9122d1263c8f8b724cb0fc97ad088
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 11:57:09 -0600

    Test: add bidders name test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe Auction do
     expect(auction.attendees).to eq([attendee1, attendee2, attendee3])
   end
 
-  it 'returns unpopular items' do
+  xit 'returns unpopular items' do
     auction = Auction.new
     item1 = Item.new('Chalkware Piggy Bank')
     item2 = Item.new('Bamboo Picture Frame')
@@ -58,11 +58,67 @@ RSpec.describe Auction do
     auction.add_attendee(attendee1)
     auction.add_attendee(attendee2)
     auction.add_attendee(attendee3)
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    expect(item1.bids).to eq({})
     item1.add_bid(attendee2, 20)
     item1.add_bid(attendee1, 22)
     item4.add_bid(attendee3, 50)
-    expect(auction.unpopular_items).to eq([item2, item3, item5])
+    expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
 
+    expect(auction.unpopular_items).to eq([item2, item3, item5])
+  end
+
+  xit 'can return potential revenue' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    expect(auction.attendees).to eq([])
+    auction.add_attendee(attendee1)
+    auction.add_attendee(attendee2)
+    auction.add_attendee(attendee3)
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    expect(item1.bids).to eq({})
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    expect(auction.potential_revenue).to eq(87)
+  end
+
+  it 'can return bidders names' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    expect(auction.bidders).to eq(["Megan", "Bob", "Mike"])
   end
 
 end
