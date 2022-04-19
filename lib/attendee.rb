aa9a1d38cab4112b9457f9b3511d645d759d03e1
Andrew King <dev.andrewking@gmail.com>
2022-04-19 11:10:30 -0500

    add init and tests for attendee

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -1,4 +1,11 @@
 class Attendee
+  attr_reader :name,
+              :budget
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @budget = attributes[:budget]
+  end
 
 
 end
--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -2,11 +2,13 @@ require './lib/attendee'
 RSpec.describe Attendee do
 
   it 'exists' do
-
+    attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    expect(attendee).to be_a(Attendee)
   end
 
   it 'has attributes' do
-
+    attendee = Attendee.new({name: 'Megan', budget: '$50'})
+    expect(attendee.name).to eq('Megan')
   end
 
 end
