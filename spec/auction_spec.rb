b3c5f3d30b78d5fd51183f400a6a01869a919629
Jim Riddle <jimriddle0@gmail.com>
2022-04-19 11:13:19 -0500

    wrote test for giving me item names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -25,4 +25,15 @@ RSpec.describe Auction do
       expect(auction.items).to eq([item1, item2])
     end
 
+    it 'can give me the items names' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+
+      auction = Auction.new
+      auction.add_item(item1)
+      auction.add_item(item2)
+
+      expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+
 end
