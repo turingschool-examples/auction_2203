a619af932afae029b957c63a58e35d912216688e
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-19 09:31:56 -0700

    Add attendee method to correct file

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,8 +1,1 @@
-class Attendee
-  attr_reader :name,
-              :budget
-  def initialize(attributes)
-    @name = attributes[:name]
-    @budget = attributes[:budget].delete("$").to_i
-  end
-end
+
