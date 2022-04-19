b6b03a57f76b9ea5190b92e6534204c6415062c5
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 13:13:17 -0400

    "Test: Add test for close bidding"

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -44,4 +44,21 @@ describe Item do
       expect(@item1.current_high_bid).to eq(22)
     end
   end
+  context 'Iteration 3' do
+    before(:each) do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      @item1.add_bid(@attendee1, 22)
+      @item1.add_bid(@attendee2, 20)
+    end
+
+    it 'can close bidding' do
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+      @item1.close_bidding
+      @item1.add_bid(@attendee3, 70)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+    end
+  end
 end
