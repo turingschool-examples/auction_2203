4d35255cae1fe1994f9c208fdc39e9de60a9144b
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-19 10:34:04 -0600

    Test: can bid on Item

--- lib/auction.rb
+++ lib/auction.rb
@@ -15,3 +15,14 @@ class Auction
     end
   end
 end
+
+
+# Graded Items:
+#  1. Add a bid to an Item: `Item #add_bid`
+#  1. Return the current high bid of an Item: `Item #current_high_bid`
+#  1. Return the list of unpopular items from an Auction: `Auction #unpopular_items`
+#  1. Return the potential revenue from an Auction: `Auction #potential_revenue`
+#
+# Use TDD to update your classes to respond to the following interaction pattern.  Some notes:
+# - `unpopular_items` are those items which have no bids.
+# - `potential_revenue` is the total possible sale price of the items (the items highest bid)
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,7 +5,12 @@ require './lib/attendee'
 describe Item do
   let(:item1) { Item.new('Chalkware Piggy Bank') }
   let(:item2) { Item.new('Bamboo Picture Frame') }
-  let(:attendee) { Attendee.new({name: 'Megan', budget: '$50'}) }
+  let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }
+  let(:item4) { Item.new('2 Days Dogsitting') }
+  let(:item5) { Item.new('Forever Stamps') }
+  let(:attendee1) { Attendee.new({name: 'Megan', budget: '$50'}) }
+  let(:attendee2) { Attendee.new({name: 'Bob', budget: '$75'}) }
+  let(:attendee3) { Attendee.new({name: 'Mike', budget: '$100'}) }
   let(:auction) { Auction.new }
 
   it "exists" do
@@ -14,5 +19,15 @@ describe Item do
 
   it "has attributes" do
     expect(item1.name).to eq('Chalkware Piggy Bank')
+  end
+
+  it "can add items" do
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    expect(item1.bids).to eq []
   end
 end
