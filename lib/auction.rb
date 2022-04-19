d8a844f9a9e23a9a8bcaaaadfccaa3d42b68a675
Sage Skaff <skaffsage@gmail.com>
2022-04-19 09:25:59 -0700

    Add add_item method

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,7 +1,14 @@
+require "./lib/item"
+require "./lib/attendee"
+
 class Auction
   attr_reader :items
 
   def initialize
     @items = []
+  end
+
+  def add_item(item)
+    items << item
   end
 end
