650af29d9be323c32a119d28f89a85c554ac705e
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 10:22:15 -0600

    Feat: item names method

--- lib/auction.rb
+++ lib/auction.rb
@@ -6,6 +6,16 @@ class Auction
   end
 
   def add_item(item)
-    @item << item
+    @items << item
   end
+
+  def item_names
+    item_arr = []
+    @items.each do |item|
+      item_arr << item.name
+      # require "pry"; binding.pry
+    end
+    item_arr
+  end
+
 end
