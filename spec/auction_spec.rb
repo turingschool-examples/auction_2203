387e1e6e77af5cd7cd9f814f9a0b7229fc6e79d1
Andrew King <dev.andrewking@gmail.com>
2022-04-19 11:18:44 -0500

    add item_names test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -21,4 +21,10 @@ RSpec.describe Auction do
     expect(@auction.items).to eq([@item1, @item2])
   end
 
+  it 'can access item names' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+  end
+
 end
