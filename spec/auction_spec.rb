70f25f93a33b1bd4416fe8efa917ad0bd3cceef1
Andrew King <dev.andrewking@gmail.com>
2022-04-19 11:16:04 -0500

    add add_item test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,14 +1,24 @@
 require './lib/auction'
+require './lib/item'
 RSpec.describe Auction do
+  before :each do
+    @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
 
   it 'exists' do
-    auction = Auction.new
-    expect(auction).to be_a(Auction)
+    expect(@auction).to be_a(Auction)
   end
 
   it 'has attributes' do
-    auction = Auction.new
-    expect(auction.items).to eq([])
+    expect(@auction.items).to eq([])
+  end
+
+  it 'can add items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.items).to eq([@item1, @item2])
   end
 
 end
