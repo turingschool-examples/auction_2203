e11f66e4dd9bd34c74009126ae30469b960e1f94
Deannah Burke <deannah@bierstadtlager.com>
2022-04-19 10:19:26 -0600

    Write auction spec

---
+++ spec/auction_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Auction do
+  before :each do
+    @auction = Auction.new
+
+  end
+
+  it "exists" do
+
+    expect(@auction).to be_a(Auction)
+  end
+
+  it "has attributes" do
+
+    expect(@auction.items).to eq([])
+  end
+end
