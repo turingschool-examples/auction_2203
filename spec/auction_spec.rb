73951567f6bb386751bc5bcc2dc9c2b82950d4e7
“Brad <hutch13531@gmail.com>
2022-04-19 12:43:02 -0400

    Test: Auction - item_names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -24,6 +24,17 @@ RSpec.describe Auction do
 
   expect(auction.items).to eq([item1, item2])
 
+  it 'can output item names' do
+    auction = Auction.new
+
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+  expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+
 
 
   end
