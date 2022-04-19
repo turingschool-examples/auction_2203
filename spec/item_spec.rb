32964defddcc375dc89b314391313c738d51c909
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-19 11:19:34 -0500

    Test: Completed IT1 tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require './lib/auction'
+require './lib/attendee'
+
+RSpec.describe Item do
+
+  it "exists" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    expect(item1).to be_an_instance_of(Item)
+  end
+
+  it "has readable attributes" do
+    item1 = Item.new('Chalkware Piggy Bank')
+    expect(item1.name).to eq('Chalkware Piggy Bank')
+    expect(item1.bids).to eq({})
+  end 
+end
