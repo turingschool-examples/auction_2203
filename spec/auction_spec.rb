315383f5b4ef8541732aef3122e41ec32c9bc2a1
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 12:20:05 -0400

    "Test: Add initial tests for auction"

---
+++ spec/auction_spec.rb
@@ -0,0 +1,26 @@
+require './lib/auction'
+require 'Rspec'
+
+describe Auction do
+  context 'Iteration 1' do
+    before(:each) do
+    @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+    end
+
+    it 'exists' do
+      expect(@auction).to be_an_instance_of(Auction)
+    end
+
+    it 'has readable attributes' do
+      expect(@auction.items).to eq([])
+    end
+
+    it 'can add items to auction' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.items).to eq([@item1, @item2])
+    end
+  end
+end
