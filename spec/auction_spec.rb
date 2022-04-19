4be579b713b4e9c1c3a090cfb7bfb45a34ea6a41
Jim Riddle <jimriddle0@gmail.com>
2022-04-19 11:26:02 -0500

    wrote test for finding unpopular items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -36,4 +36,37 @@ RSpec.describe Auction do
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
     end
 
+    it 'can give me unpopular items' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+      auction = Auction.new
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+
+      expect(auction.unpopular_items.count).to eq(3)
+      expect(auction.unpopular_items).to be_an_instance_of(Array)
+
+
+      item3.add_bid(attendee2, 15)
+
+      expect(auction.unpopular_items.count).to eq(2)
+
+    end
+
 end
