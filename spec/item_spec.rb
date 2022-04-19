d0cce8c54337a2e676a4593ae160769df12fee3b
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 12:36:17 -0400

    "Test: Add first set of IT2 tests"

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -16,4 +16,26 @@ describe Item do
       expect(@item1.name).to eq('Chalkware Piggy Bank')
     end
   end
+  context 'Iteration 2' do
+    before(:each) do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    end
+
+    it 'starts with no bids by default' do
+      expect(@item1.bids).to eq({})
+    end
+
+    it 'can add bids' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
+    end
+  end
 end
