4ceddaf11a60d4d75f8e6f1dd3ab77f3ff32aec4
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 12:43:11 -0400

    "Test: Add high_bid test"

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -37,5 +37,11 @@ describe Item do
       @item1.add_bid(@attendee1, 22)
       expect(@item1.bids).to eq({@attendee2 => 20, @attendee1 => 22})
     end
+
+    it 'can return the current highest offer' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      expect(@item1.current_high_bid).to eq(22)
+    end
   end
 end
