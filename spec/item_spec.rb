33335b771bb633cd25913309d33542441ce3014e
“Brad <hutch13531@gmail.com>
2022-04-19 12:08:30 -0400

    Test: Item-has readable attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Item do
     item1 = Item.new('Chalkware Piggy Bank')
   expect(item).to be_an_instance_of(Item)
   end
+
+  it 'has readable attributes' do
+    item1 = Item.new('Chalkware Piggy Bank')
+  expect(item1.name).to eq('Chalkware Piggy Bank')
+  end
+
+  
 end
