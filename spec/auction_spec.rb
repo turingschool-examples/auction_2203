e7810939aa4ed61ffabc528d20a9d1107bbd485a
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-19 12:57:37 -0500

    Test: added test for bidders and bidders_info

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -117,4 +117,66 @@ RSpec.describe Auction do
     item3.add_bid(attendee2, 15)
     expect(auction.potential_revenue).to eq(87)
   end
+
+  it "can give names of bidders" do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    expect(auction.bidders).to eq(["Bob","Megan", "Mike"])
+    # require 'pry';binding.pry
+  end
+
+  it "can give info on each person who has bid" do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    expected = {
+       attendee1 =>
+         {
+           :budget => 50,
+           :items => ['Chalkware Piggy Bank']
+         },
+       attendee2 =>
+         {
+           :budget => 75,
+           :items => ['Chalkware Piggy Bank','Homemade Chocolate Chip Cookies']
+         },
+       attendee3 =>
+         {
+           :budget => 100,
+           :items => ['2 Days Dogsitting']
+         }
+      }
+    expect(auction.bidder_info).to eq(expected)
+  end
 end
