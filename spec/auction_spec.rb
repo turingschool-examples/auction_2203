d27fe3028078e4be23413a2af4c5ab9d666fc2c1
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-19 10:37:16 -0600

    Fix: move bid test to auction_spec.rb

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -29,4 +29,29 @@ describe Auction do
 
     expect(auction.item_names).to eq ["Chalkware Piggy Bank", "Bamboo Picture Frame"]
   end
+
+  let(:item1) { Item.new('Chalkware Piggy Bank') }
+  let(:item2) { Item.new('Bamboo Picture Frame') }
+  let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }
+  let(:item4) { Item.new('2 Days Dogsitting') }
+  let(:item5) { Item.new('Forever Stamps') }
+  let(:attendee1) { Attendee.new({name: 'Megan', budget: '$50'}) }
+  let(:attendee2) { Attendee.new({name: 'Bob', budget: '$75'}) }
+  let(:attendee3) { Attendee.new({name: 'Mike', budget: '$100'}) }
+  let(:auction) { Auction.new }
+
+  it "Attendees can bid on items" do
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    expected = {
+      attendee2 => 20,
+      attendee1 => 22
+    }
+
+    expect(item1.bids).to eq(expected)
+  end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,15 +3,10 @@ require './lib/auction'
 require './lib/attendee'
 
 describe Item do
+  let(:auction) { Auction.new }
   let(:item1) { Item.new('Chalkware Piggy Bank') }
   let(:item2) { Item.new('Bamboo Picture Frame') }
-  let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }
-  let(:item4) { Item.new('2 Days Dogsitting') }
-  let(:item5) { Item.new('Forever Stamps') }
-  let(:attendee1) { Attendee.new({name: 'Megan', budget: '$50'}) }
-  let(:attendee2) { Attendee.new({name: 'Bob', budget: '$75'}) }
-  let(:attendee3) { Attendee.new({name: 'Mike', budget: '$100'}) }
-  let(:auction) { Auction.new }
+  let(:attendee) { Attendee.new({name: 'Megan', budget: '$50'}) }
 
   it "exists" do
     expect(item1).to be_a Item
@@ -19,15 +14,5 @@ describe Item do
 
   it "has attributes" do
     expect(item1.name).to eq('Chalkware Piggy Bank')
-  end
-
-  it "can add items" do
-    auction.add_item(item1)
-    auction.add_item(item2)
-    auction.add_item(item3)
-    auction.add_item(item4)
-    auction.add_item(item5)
-
-    expect(item1.bids).to eq []
   end
 end
