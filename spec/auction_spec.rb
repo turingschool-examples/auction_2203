adad034005f386bd7b31f27e41fcdb36a18feb88
Lucas Colwell <colwellc37@gmail.com>
2022-04-19 11:17:28 -0600

    Test: write unpop item test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -65,5 +65,13 @@ RSpec.describe Auction do
 
       expect(@item1.current_high_bid).to eq(22)
     end
+
+    it 'can tell unpopular items' do
+      @item1.add_bid(@attendee2, 20)
+      @item1.add_bid(@attendee1, 22)
+      @item4.add_bid(@attendee3, 50)
+
+      expect(@auction.unpopular_items).to eq([@item2, @item3, @item5])
+    end
   end
 end
