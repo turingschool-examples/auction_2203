b5bda9d4f832ca31784719f722c70d26256bac14
Bryce Wein <bryce.wein@gmail.com>
2022-04-19 12:15:13 -0400

    Test: add item test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -4,9 +4,21 @@ require './lib/item'
 require './lib/auction'
 
 describe Auction do
-  it 'exists with attributes' do
-    auction = Auction.new
-    expect(auction).to be_a(Auction)
-    expect(auction.items).to eq([])
+  describe 'Itteration1' do
+    before :each do
+      @auction = Auction.new
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+    end
+    it 'exists with attributes' do
+      expect(@auction).to be_a(Auction)
+      expect(@auction.items).to eq([])
+    end
+
+    it 'can add items' do
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      expect(@auction.items).to eq([@item1, @item2])
+    end
   end
 end
