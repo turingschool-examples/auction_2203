4d31065db215398a66ee209a25995907ae59afe1
Wesley Garcia <wes@alltradestech.com>
2022-04-19 09:07:58 -0700

    Test add: Item class exists

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require "pry"
+require "./lib/item"
+require "./lib/auction"
+require "./lib/attendee"
+
+RSpec.describe Item do
+  let(:item1) { Item.new("Chalkware Piggy Bank") }
+
+  it "exists and has attributes" do
+    expect(item1).to be_a Item
+    expect(item1.name).to eq("Chalkware Piggy Bank")
+  end
+end
