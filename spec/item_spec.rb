82986c268d0695db637ef2f27ba221465b56bc8f
“Brad <hutch13531@gmail.com>
2022-04-19 12:21:34 -0400

    Feat: Item - Class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,7 +3,7 @@ require './lib/item'
 RSpec.describe Item do
   it 'exist' do
     item1 = Item.new('Chalkware Piggy Bank')
-  expect(item).to be_an_instance_of(Item)
+  expect(item1).to be_an_instance_of(Item)
   end
 
   it 'has readable attributes' do
