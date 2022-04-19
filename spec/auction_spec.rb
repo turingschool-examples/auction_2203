4e84a0a69e0d54376c4885d8a44212d8f4c88f12
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 10:54:26 -0600

    Test: correct add bit test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -45,27 +45,4 @@ RSpec.describe Auction do
   end
 
 
-  xit 'can add bids' do
-    auction = Auction.new
-    item1 = Item.new('Chalkware Piggy Bank')
-    item2 = Item.new('Bamboo Picture Frame')
-    item3 = Item.new('Homemade Chocolate Chip Cookies')
-    item4 = Item.new('2 Days Dogsitting')
-    item5 = Item.new('Forever Stamps')
-    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
-    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
-    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
-
-    auction.add_item(item1)
-    auction.add_item(item2)
-    auction.add_item(item3)
-    auction.add_item(item4)
-    auction.add_item(item5)
-    expect(auction.bids).to eq({})
-    item1.add_bid(attendee2, 20)
-    item1.add_bid(attendee1, 22)
-    expect(auctiion.bids).to eq({attendee2 => 20, attendee1 => 22})
-  end
-
-
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Item do
    expect(item1).to be_an_instance_of(Item)
    expect(item1.name).to eq("Chalkware Piggy Bank")
   end
+
+  it 'can add bids' do
+    auction = Auction.new
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+    expect(item1.bids).to eq({})
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+  end
+
 end
