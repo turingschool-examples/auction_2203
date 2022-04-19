dd157822c0a97e974d52c2bcd9baed5a8eb5b7b2
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 12:15:30 -0400

    "Feat: Add attendee class, pass tests, add readable whitespace to tests"

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -6,9 +6,11 @@ describe Attendee do
     before(:each) do
     @attendee = Attendee.new({name: 'Megan', budget: '$50'})
     end
+
     it 'exists' do
       expect(@attendee).to be_an_instance_of(Attendee)
     end
+
     it 'has readable attributes' do
       expect(@attendee.name).to eq('Megan')
       expect(@attendee.budget).to eq(50)
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,9 +7,11 @@ describe Item do
       @item1 = Item.new('Chalkware Piggy Bank')
       @item2 = Item.new('Bamboo Picture Frame')
     end
+
     it 'exists' do
       expect(@item1).to be_an_instance_of(Item)
     end
+
     it 'has a readable name' do
       expect(@item1.name).to eq('Chalkware Piggy Bank')
     end
