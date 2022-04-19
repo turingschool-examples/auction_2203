049c9f30ff8671d977b706bae31f52ea4f12f084
Thiago Silveira <thiago1992@yahoo.com>
2022-04-19 11:30:31 -0600

    Feat: Add bid tests and the methods add_bid and current_high_bid for IT2.

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,8 +5,16 @@ require './lib/attendee'
 RSpec.describe Auction do
   before :each do
     @auction = Auction.new
+
     @item1 = Item.new('Chalkware Piggy Bank')
     @item2 = Item.new('Bamboo Picture Frame')
+    @item3 = Item.new('Homemade Chocolate Chip Cookies')
+    @item4 = Item.new('2 Days Dogsitting')
+    @item5 = Item.new('Forever Stamps')
+
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+    @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+    @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
   end
 
   it 'exists and has attributes' do
@@ -25,4 +33,28 @@ RSpec.describe Auction do
     @auction.add_item(@item2)
     expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
+
+  it 'places bids on item and can return bids on item' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@item1.bids).to eq({})
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @expected = {
+                  @attendee2 => 20,
+                  @attendee1 => 22
+                }
+    expect(@item1.bids).to eq(@expected)
+  end
+
+  it 'can return an items highest placed bid' do
+    @auction.add_item(@item1)
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    expect(@item1.current_high_bid).to eq(22)
+  end
+
+  # @auction.add_item(@item3)
+  # @auction.add_item(@item4)
+  # @auction.add_item(@item5)
 end
