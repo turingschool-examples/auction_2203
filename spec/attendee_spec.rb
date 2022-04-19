a7e76eda6b44f845b60751a355de429ffac812a8
“Brad <hutch13531@gmail.com>
2022-04-19 12:28:35 -0400

    Test: Attendee - Has readable attributes

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Attendee do
     attendee = Attendee.new({name: 'Megan', budget: '$50'})
   expect(attendee).to be_an_instance_of(Attendee)
   end
+
+  it 'has readable attributes' do
+    attendee = Attendee.new({name: 'Megan', budget: '$50'})
+  expect(attendee.name).to eq('Megan')
+  expect(attendee.budget).to eq('$50')
+  end
 end
