bcd355b18eac3b4e9e8a54e32e676392cf01faf8
Deannah Burke <deannah@bierstadtlager.com>
2022-04-19 10:28:29 -0600

    Complete Iteration 1

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,7 +5,8 @@ require './lib/attendee'
 RSpec.describe Auction do
   before :each do
     @auction = Auction.new
-
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
   it "exists" do
@@ -16,5 +17,19 @@ RSpec.describe Auction do
   it "has attributes" do
 
     expect(@auction.items).to eq([])
+  end
+
+  it "can add items" do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+
+    expect(@auction.items).to eq([@item1, @item2])
+  end
+
+  it "can return item names" do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    
+    expect(@auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
   end
 end
