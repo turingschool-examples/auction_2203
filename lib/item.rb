063e1356d9c7f46848124722702c0d1e4ec751eb
tjhaines-cap <tjhaines@eckerd.edu>
2022-04-19 10:12:49 -0600

    Feat: add atendee

--- lib/item.rb
+++ lib/item.rb
@@ -2,8 +2,11 @@
 
 class Item
 
+  attr_reader :name
+
   def initialize(name)
     @name = name
+    @bids = {}
   end
-  
+
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,4 +7,9 @@ describe Item do
     item1 = Item.new('Chalkware Piggy Bank')
     expect(item1).to be_a(Item)
   end
+
+  it 'has attributes' do
+    item1 = Item.new('Chalkware Piggy Bank')
+    expect(item1.name).to eq('Chalkware Piggy Bank')
+  end
 end
