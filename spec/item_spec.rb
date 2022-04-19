f9cd4b80114815e06b8077523f9c61cd93802086
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 12:07:02 -0400

    "Test: Create initial tests for item"

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require './lib/item'
+require 'Rspec'
+
+describe Item do
+  context 'Iteration 1' do
+    before(:each) do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+    end
+    it 'exists' do
+      expect(@item1).to be_an_instance_of(Item)
+    end
+    it 'has a readable name' do
+      expect(@item1.name).to eq('Chalkware Piggy Bank')
+    end
+  end
+end
