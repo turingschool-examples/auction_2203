5c58bec0841dc98e79ebb4a7b3ee2ed2ca7ee67e
Bryce Wein <bryce.wein@gmail.com>
2022-04-19 12:22:44 -0400

    Test: add test of bids

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,5 +6,6 @@ describe Item do
     item1 = Item.new('Chalkware Piggy Bank')
     expect(item1).to be_a(Item)
     expect(item1.name).to eq('Chalkware Piggy Bank')
+    expect(item1.bids).to eq({})
   end
 end
