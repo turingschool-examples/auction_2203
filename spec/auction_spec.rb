1b5958bc692107abc7b234e0e9f84a8926999cd0
Jim Riddle <jimriddle0@gmail.com>
2022-04-19 12:15:38 -0500

    wrote test for giving me a date

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,6 +1,7 @@
 require './lib/attendee'
 require './lib/auction'
 require './lib/item'
+require 'date'
 
 RSpec.describe Auction do
 
@@ -196,6 +197,30 @@ RSpec.describe Auction do
       # require 'pry'; binding.pry
 
 
+    end
+
+    it 'can give me the auctions date' do
+      item1 = Item.new('Chalkware Piggy Bank')
+      item2 = Item.new('Bamboo Picture Frame')
+      item3 = Item.new('Homemade Chocolate Chip Cookies')
+      item4 = Item.new('2 Days Dogsitting')
+      item5 = Item.new('Forever Stamps')
+
+      attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+      auction = Auction.new
+
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      date = "19/4/2022"
+
+      expect(auction.date).to eq(date)
     end
 
 end
