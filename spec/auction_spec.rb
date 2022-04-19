25447653b3ff1cd708a959e119a433488ca15e0e
Wesley Garcia <wes@alltradestech.com>
2022-04-19 09:26:35 -0700

    Test add: Auction can list item names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -17,7 +17,12 @@ RSpec.describe Auction do
   it "can add and store items" do
     auction.add_item(item1)
     auction.add_item(item2)
-
     expect(auction.items).to eq([item1, item2])
+  end
+
+  it "can list items by name" do
+    auction.add_item(item1)
+    auction.add_item(item2)
+    expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
 end
