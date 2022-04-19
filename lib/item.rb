3300c59ad00573c6c7791afbe0a8b8814583c2f2
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 10:55:29 -0600

    Feat: add id method

--- lib/item.rb
+++ lib/item.rb
@@ -1,11 +1,15 @@
 
 class Item
-  attr_reader :name
-
-
+  attr_reader :name,
+              :bids
+              # :attendees
   def initialize(name)
     @name = name
+    @bids = {}
   end
 
-  
+  def add_bid(attendee, amount)
+      bids[attendee] = amount
+    return bids
+  end
 end
