cda2f8caa7bf9e02b9fa12b055005c49adeecf5a
Bryce Wein <bryce.wein@gmail.com>
2022-04-19 12:04:20 -0400

    Feat: Add Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require './lib/item'
 
-desscribe Item do
+describe Item do
   it 'exists with attributes' do
     item1 = Item.new('Chalkware Piggy Bank')
     expect(item1).to be_a(Item)
