82e2073dedaf5c6c957c2db0da34d53d233d25e0
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-19 14:28:46 -0600

    Test: Auction returns potential revenue

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -54,4 +54,18 @@ describe Auction do
 
     expect(auction.unpopular_items).to eq [item2, item3, item5]
   end
+
+  it "returns potential revenue" do
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+
+    expect(auction.potential_revenue).to eq(87)
+  end
 end
