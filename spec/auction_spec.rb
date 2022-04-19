d575542159a421aa61688cd710271eb004d51bb0
Andrew King <dev.andrewking@gmail.com>
2022-04-19 12:03:42 -0500

    add bidders test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -60,6 +60,19 @@ RSpec.describe Auction do
     expect(@auction.potential_revenue).to eq(87)
   end
 
+  it 'can list bidders' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+    expect(@auction.bidders).to eq(["Megan", "Bob", "Mike"])
+  end
+
 
 
 end
