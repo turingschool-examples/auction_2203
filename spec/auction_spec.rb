5c1a0095fc580d6fea4fe403b4cc20632149ead6
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-19 10:26:52 -0600

    Test: Auction lists item names

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -22,4 +22,11 @@ describe Auction do
 
     expect(auction.items).to eq [item1, item2]
   end
+
+  it "has item names" do
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    expect(auction.item_names).to eq ["Chalkware Piggy Bank", "Bamboo Picture Frame"]
+  end
 end
