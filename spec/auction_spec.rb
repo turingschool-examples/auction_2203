8de3bd20af72b54486f3de4903d8faee2c34831a
Andrew King <dev.andrewking@gmail.com>
2022-04-19 11:55:27 -0500

    add potential revenue test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -35,11 +35,29 @@ RSpec.describe Auction do
   end
 
   it 'can have unpopular items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
     @item1.add_bid(@attendee2, 20)
     @item1.add_bid(@attendee1, 22)
     @item4.add_bid(@attendee3, 50)
     @item3.add_bid(@attendee2, 15)
     expect(@auction.unpopular_items).to eq([@item2, @item5])
+  end
+
+  it 'can have potential revenue' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    @auction.add_item(@item3)
+    @auction.add_item(@item4)
+    @auction.add_item(@item5)
+    @item1.add_bid(@attendee2, 20)
+    @item1.add_bid(@attendee1, 22)
+    @item4.add_bid(@attendee3, 50)
+    @item3.add_bid(@attendee2, 15)
+    expect(@auction.potential_revenue).to eq(87)
   end
 
 
