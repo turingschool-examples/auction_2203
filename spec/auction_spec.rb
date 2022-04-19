34e9e4f9ad7d9ba46dc95926492e6f1eb677eae1
Wesley Garcia <wes@alltradestech.com>
2022-04-19 09:42:45 -0700

    Test add: Item can track attendee bids

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe Auction do
     auction.add_item(item2)
     expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
   end
+
+  describe "Iteration 2" do
+    let(:item3) { Item.new("Homemade Chocolate Chip Cookies") }
+    let(:item4) { Item.new("2 Days Dogsitting") }
+    let(:item5) { Item.new("Forever Stamps") }
+    let(:attendee1) { Attendee.new({name: "Megan", budget: "$50"}) }
+    let(:attendee2) { Attendee.new({name: "Bob", budget: "$75"}) }
+    let(:attendee3) { Attendee.new({name: "Mike", budget: "$100"}) }
+    before :each do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+    end
+  end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,4 +11,33 @@ RSpec.describe Item do
     expect(item1).to be_a Item
     expect(item1.name).to eq("Chalkware Piggy Bank")
   end
+
+  describe "Iteration 2" do
+    let(:item1) { Item.new("Chalkware Piggy Bank") }
+    let(:item2) { Item.new("Bamboo Picture Frame") }
+    let(:attendee) { Attendee.new({name: "Megan", budget: "$50"}) }
+    let(:auction) { Auction.new }
+    let(:item3) { Item.new("Homemade Chocolate Chip Cookies") }
+    let(:item4) { Item.new("2 Days Dogsitting") }
+    let(:item5) { Item.new("Forever Stamps") }
+    let(:attendee1) { Attendee.new({name: "Megan", budget: "$50"}) }
+    let(:attendee2) { Attendee.new({name: "Bob", budget: "$75"}) }
+    let(:attendee3) { Attendee.new({name: "Mike", budget: "$100"}) }
+    before :each do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+    end
+
+    it "can track and add attendee bids" do
+      expect(item1.bids).to eq({})
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+
+      expect(item1.bids).to eq({attendee2 => 20, attendee1 => 22})
+    end
+  end
 end
