3d4efe7ea77eec92bfd3590336878f388c660449
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-19 10:09:02 -0600

    Add Attendee

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Item do
 
     it 'exists and has attributes' do 
         item1 = Item.new('Chalkware Piggy Bank')
+        item2 = Item.new('Bamboo Picture Frame')
 
         expect(item1).to be_a(Item)
         expect(item1.name).to eq('Chalkware Piggy Bank')
