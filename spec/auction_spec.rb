c2f34923cc6f8f0b543cf864b2de3df8f5b9ff6b
Sarah Hall <swatson1994@yahoo.com>
2022-04-19 13:45:42 -0400

    "Test: Add close_auction test"

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -97,4 +97,40 @@ describe Auction do
     expect(@auction.bidder_info).to eq(expected)
     end
   end
+
+  context 'Iteration 4' do
+    before(:each) do
+      @item1 = Item.new('Chalkware Piggy Bank')
+      @item2 = Item.new('Bamboo Picture Frame')
+      @item3 = Item.new('Homemade Chocolate Chip Cookies')
+      @item4 = Item.new('2 Days Dogsitting')
+      @item5 = Item.new('Forever Stamps')
+      @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+      @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+      @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+      @auction = Auction.new
+      @auction.add_item(@item1)
+      @auction.add_item(@item2)
+      @auction.add_item(@item3)
+      @auction.add_item(@item4)
+      @auction.add_item(@item5)
+      @item1.add_bid(@attendee1, 22)
+      @item1.add_bid(@attendee2, 20)
+      @item4.add_bid(@attendee2, 30)
+      @item4.add_bid(@attendee3, 50)
+      @item3.add_bid(@attendee2, 15)
+      @item5.add_bid(@attendee1, 35)
+    end
+
+    it 'can close an auction and sell items to highest bidder' do
+      expect(@auction.close_auction).to be_a(Hash)
+
+      expected = { @item1 => @attendee2,
+      @item2 => 'Not Sold',
+      @item3 => @attendee2,
+      @item4 => @attendee3,
+      @item5 => @attendee1}
+      expect(@auction.close_auction).to eq(expected)
+    end
+  end
 end
