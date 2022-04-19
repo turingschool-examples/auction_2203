421dc0fc2253cd332be1dcb60c70a973ae8f36d7
Lucas Colwell <colwellc37@gmail.com>
2022-04-19 10:10:17 -0600

    Test: write item tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/item'
+
+RSpec.describe Item do
+  before :each do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+  end
+
+  it 'has attributes' do
+    expect(@item1.name).to eq("Chalkware Piggy Bank")
+  end
+end
