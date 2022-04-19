a6c80cf4e4115795837df08b709cf9b2ae68ff38
Shirley DeCesari <shirley.decesari@gmail.com>
2022-04-19 10:43:44 -0700

    Add test for name auction items

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,6 +5,8 @@ require './lib/attendee'
 RSpec.describe Auction do
   before :each do
     @auction = Auction.new
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
   end
 
   it 'exists and has attributes' do
@@ -16,5 +18,11 @@ RSpec.describe Auction do
     @auction.add_item(@item1)
     @auction.add_item(@item2)
     expect(@auction.items).to eq [@item1, @item2]
+  end
+
+  it 'can name auction items' do
+    @auction.add_item(@item1)
+    @auction.add_item(@item2)
+    expect(@auction.item_names).to eq ["Chalkware Piggy Bank", "Bamboo Picture Frame"]
   end
 end
