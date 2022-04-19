953f1755cf7c73f2a9d6b1b67f2be96d79507a2e
Bryce Wein <bryce.wein@gmail.com>
2022-04-19 12:39:13 -0400

    Feat: Potential revenue method

--- lib/auction.rb
+++ lib/auction.rb
@@ -16,4 +16,8 @@ class Auction
   def unpopular_items
     @items.select { |item| item.bids.length == 0 }
   end
+
+  def potential_revenue
+    (@items - unpopular_items).map { |item| item.current_high_bid }.sum
+  end
 end
