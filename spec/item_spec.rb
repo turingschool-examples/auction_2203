fdadd35727b91fe8c2ae46c1f5cbcb5a79abd1a1
Sage Skaff <skaffsage@gmail.com>
2022-04-19 09:11:25 -0700

    Add Item initialize

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe Item do
       item1 = Item.new('Chalkware Piggy Bank')
       expect(item1).to be_a Item
     end
+
+    it "has readable attributes" do
+      item1 = Item.new('Chalkware Piggy Bank')
+      expect(item1.name).to eq 'Chalkware Piggy Bank'
+    end
   end
 end
