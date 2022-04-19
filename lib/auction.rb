fe06d245fd53800a86e825cff2a7bf58c5a7a8e2
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-19 10:38:59 -0600

    Finish iteration 2

--- lib/auction.rb
+++ lib/auction.rb
@@ -16,4 +16,9 @@ attr_reader :items
     def unpopular_items
         items.find_all { |item| item.bids == {} }
     end
+
+    def potential_revenue
+        #the total of all the items highests bids - take into account current_high_bid
+        items.map { |item| item.current_high_bid }.compact!.sum
+    end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -49,4 +49,32 @@ RSpec.describe Auction do
 
         expect(auction.unpopular_items).to eq([item2, item3, item5])
     end
+
+    it 'can calculate the potential revenue' do
+        item1 = Item.new('Chalkware Piggy Bank')
+        item2 = Item.new('Bamboo Picture Frame')
+        item3 = Item.new('Homemade Chocolate Chip Cookies')
+        item4 = Item.new('2 Days Dogsitting')
+        item5 = Item.new('Forever Stamps')
+
+        attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
+        auction = Auction.new
+        auction.add_item(item1)
+        auction.add_item(item2)
+        auction.add_item(item3)
+        auction.add_item(item4)
+        auction.add_item(item5)
+
+        expect(item1.bids).to eq ({})
+
+        item1.add_bid(attendee2, 20)
+        item1.add_bid(attendee1, 22)
+        item4.add_bid(attendee3, 50)
+        item3.add_bid(attendee2, 15)
+        
+        expect(auction.potential_revenue).to eq(87)
+    end
 end
