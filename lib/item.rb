9562468c981cf47899353dbde00b7c3b329d1121
Nick Jacobs <nickjacobsatc@gmail.com>
2022-04-19 15:12:50 -0500

    Fix: Fixed close bids method

--- lib/item.rb
+++ lib/item.rb
@@ -4,10 +4,11 @@ class Item
 def initialize(name, bids = {})
   @name = name
   @bids = bids
+  @bidding_open = true
 end
 
   def add_bid(attendee, budget)
-    @bids[attendee] = budget
+    @bids[attendee] = budget if @bidding_open
   end
 
   def current_high_bid
@@ -15,6 +16,6 @@ end
   end
 
   def close_bidding
-
+    @bidding_open = false
   end
 end
