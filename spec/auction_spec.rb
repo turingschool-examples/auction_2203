daffbfea1c537c7d99e8d29dce606b43cd5bc8b4
“Brad <hutch13531@gmail.com>
2022-04-19 12:39:34 -0400

    Test: Auction - can add items to empty array

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -8,4 +8,25 @@ RSpec.describe Auction do
   expect(auction).to be_an_instance_of(Auction)
   end
 
+  it ' has readable attributes' do
+    auction = Auction.new
+  expect(auction.items).to eq([])
+  end
+
+  it 'can add items to empty array' do
+    auction = Auction.new
+
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+  expect(auction.items).to eq([item1, item2])
+
+
+
+  end
+
+
 end
