3b9e5d3ddaea4db8b06b687eb8bb74e57119ec11
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-25 18:00:47 -0600

    Test: returns the highest bid on an item

--- spec/2iteration_spec.rb
+++ spec/2iteration_spec.rb
@@ -40,6 +40,19 @@ describe "Iteration 1" do
         }
       expect(item1.bids).to eq expected_hash
     end
+
+    it "returns the highest bid on an item" do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+
+      expect(item1.current_high_bid).to eq(22)
+    end
   end
 
   describe Attendee do
